634e48c14d71e231ba62bea6aedfe9de51e28cd6
Uday Singh <uday144@gmail.com>
2024-04-02 15:23:01 +1100

    Text Compose with various options

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -9,26 +9,38 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Text(text = "Hello Compose")
+            Text(text = "Hello Compose")
         }
     }
 }
 
 
 @Composable
-fun helloCompose(name: String = "Compose"){
- Text(text = "Hello $name")
+fun helloCompose(name: String = "Compose") {
+    Text(text = "Hello $name")
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun previewCompose(){
-    helloCompose("Compose")
+fun previewCompose() {
+    Text(
+        text = "Hello Compose",
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        color = Color.Blue,
+        fontSize = 36.sp,
+        textAlign = TextAlign.Right
+    )
 }
