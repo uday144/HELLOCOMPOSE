d84ac31eafa5d1b98d4cd68016fc0c4d91ce2a25
Uday Singh <uday144@gmail.com>
2024-04-02 16:01:47 +1100

    TextField, state, remember

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hellocompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -29,30 +34,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Compose")
+            textInput()
         }
     }
 }
 
 
-@Composable
-fun helloCompose(name: String = "Compose") {
-    Text(text = "Hello $name")
-}
-
-@Preview(widthDp = 150, heightDp = 50)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(widthDp = 300, heightDp = 500)
 @Composable
 fun previewCompose() {
-    Button(
-        onClick = { }, colors = ButtonDefaults.buttonColors(
-            contentColor = Color.White
-        ),
-        enabled = false
-    ) {
-        Text(text = "Click me")
-        Image(
-            painter = painterResource(id = R.drawable.arrow), contentDescription = "arrow button",
-            Modifier.rotate(180F)
-        )
-    }
+    TextField(
+        value = "Hello Compose", onValueChange = {},
+        label = { Text(text = "Enter massage") },
+        placeholder = {}
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun textInput() {
+    val state = remember { mutableStateOf("") }
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+        },
+        label = { Text(text = "Enter massage") },
+        placeholder = {}
+    )
 }
